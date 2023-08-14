bbb9758ad09b404d1c04d0e765fda1d371c71384
Swastika_Pandit <swastikapandit02@gmail.com>
2023-08-14 15:55:50 +0600

    connected frontend, backend and database

---
+++ server.js
@@ -0,0 +1,67 @@
+const oracledb = require('oracledb');
+const express = require('express');
+const port = 3000;
+
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+const app = express();
+
+app.listen(port);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+async function runQuery(query) {
+    const connection = await oracledb.getConnection({
+        user: "rokomari",
+        password: "12345",
+        connectionString: "localhost/ORCLPDB"
+    });
+
+    const result = await connection.execute(query);
+
+    await connection.close();
+
+    return result;
+}
+
+app.get('/', async (req, res) => {
+    const data = await runQuery(`Select Book.TITLE, Book.ID, Book.PUBLISHER, Writer.name As WRITER_NAME from Book, Writer where (Book.Writer_id = Writer.id)`);
+    const books = data.rows;
+    // books.forEach(book => {
+    //     console.log(book.TITLE);
+    //     console.log(book.AUTHOR);
+    // })
+    res.render('index', {books: books});
+})
+
+app.get('/all_writers', async(req, res) => {
+    
+    console.log('single writer called');
+    const data = await runQuery(`SELECT NAME, ID FROM WRITER ORDER BY NAME`);
+    const writers = data.rows;
+    res.render('all_writers', {writers : writers});
+});
+
+app.get('/all_publishers', async(req, res) => {
+    const data = await runQuery(`SELECT NAME FROM PUBLISHER ORDER BY NAME`);
+    const publishers = data.rows;
+    res.render('all_publishers', {publishers});
+});
+
+app.get('/all_books/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const data = await runQuery(`SELECT Book.title, Book.GENRE, BOOK.QUANTITY_IN_STOCK,Book.Publisher, BOOK.PRICE, BOOK.DATE_PUBLISHED, Book.PAGE, WRITER.NAME AS WRITER_NAME FROM BOOK , WRITER
+    WHERE (BOOK.id = ${id}) AND (BOOK.WRITER_ID = WRITER.id )`)
+
+    res.render('single_book', { books: data.rows});
+});
+
+app.get('/all_writers/:id', async(req, res) => {
+    const id = req.params.id;
+
+    const data = await runQuery(`SELECT * FROM WRITER WHERE id = ${id}`);
+
+    console.log(data);
+    res.render('single_writer', {writers: data.rows});
+})
