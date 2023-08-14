6df8e279c943b6554d61b5f98a36d57abe3476ad
Swastika_Pandit <swastikapandit02@gmail.com>
2023-08-14 21:36:58 +0600

    showed all books of writers and publishers

--- server.js
+++ server.js
@@ -35,10 +35,9 @@ app.get('/', async (req, res) => {
 })
 
 app.get('/all_writers', async(req, res) => {
-    
-    console.log('single writer called');
-    const data = await runQuery(`SELECT NAME, ID FROM WRITER ORDER BY NAME`);
-    const writers = data.rows;
+    const writer_data = await runQuery(`SELECT NAME, ID FROM WRITER ORDER BY NAME`);
+    const writers = writer_data.rows;
+
     res.render('all_writers', {writers : writers});
 });
 
@@ -58,10 +57,18 @@ app.get('/all_books/:id', async (req, res) => {
 });
 
 app.get('/all_writers/:id', async(req, res) => {
+    console.log(req.params);
     const id = req.params.id;
+    const writer_data = await runQuery(`SELECT * FROM WRITER WHERE id = ${id}`);
+    const book_data = await runQuery(`Select Book.TITLE, Book.ID, Book.PUBLISHER, Writer.name As WRITER_NAME from Book, Writer where (Book.Writer_id = Writer.id) AND (Book.Writer_id = ${id})`);
 
-    const data = await runQuery(`SELECT * FROM WRITER WHERE id = ${id}`);
+    res.render('single_writer', {writers: writer_data.rows, books: book_data.rows});
+});
 
-    console.log(data);
-    res.render('single_writer', {writers: data.rows});
-})
+app.get('/all_publishers/:id', async(req, res) => {
+    const id = req.params.id;
+    const publisher_data = await runQuery(`Select * from publisher where name = '${id}'`);
+    const book_data = await runQuery(`Select Book.TITLE, Book.ID, Book.PUBLISHER, Writer.name As WRITER_NAME from Book, Writer where (Book.Writer_id = Writer.id) AND (Book.PUBLISHER = '${id}')`);
+
+    res.render('single_publisher', {publishers: publisher_data.rows, books: book_data.rows});
+});
